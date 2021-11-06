5a1d8793910f3bbad37f0b0a7bf3a912723f6c77
Chia Kuo <yichia520@gmail.com>
2021-11-06 23:42:39 +0800

    initial commit

--- app/src/main/java/com/chia/guess/MainActivity.kt
+++ app/src/main/java/com/chia/guess/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val secretNumber = SecretNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        R.string.ok
+
 
     }
 }
