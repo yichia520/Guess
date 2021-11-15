50ea554de7e1b18def2fbf27b860e34923476980
Chia Kuo <yichia520@gmail.com>
2021-11-15 22:02:33 +0800

    android commit

--- app/src/main/java/com/chia/guess/MainActivity.kt
+++ app/src/main/java/com/chia/guess/MainActivity.kt
@@ -2,6 +2,11 @@ package com.chia.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("MainActivity"," secret:" + secretNumber.secret)
 
+    }
+
+    fun check (view : View){
+        val n = number.text.toString().toInt()
+        println("number : ${n}")
+        Log.d("MainActivity", "number:" + n)
+        val diff = secretNumber.validate(n)
+        var message = "You Got It"
+        if(diff<0){
+            message = "Bigger"
+        }else if(diff>0){
+            message = "Smaller"
+        }
+        //Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this).
+        setTitle("Message").
+        setMessage(message).
+        setPositiveButton("ok",null).
+        show()
     }
 }
